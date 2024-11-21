539ff5f5d26050516a222d1063f6014c857ebd8c
Ramis <ramis@Ramiss-Air.Dlink>
2024-11-21 16:44:45 +0300

    git init

---
+++ task1/solution.py
@@ -0,0 +1,26 @@
+# Необходимо реализовать декоратор @strict 
+# Декоратор проверяет соответствие типов переданных в вызов функции аргументов типам аргументов, 
+# объявленным в прототипе функции. 
+# (подсказка: аннотации типов аргументов можно получить из атрибута объекта функции func.__annotations__ 
+# или с помощью модуля inspect) 
+# При несоответствии типов бросать исключение TypeError 
+# Гарантируется, что параметры в декорируемых функциях будут следующих типов: bool, int, float, str
+# Гарантируется, что в декорируемых функциях не будет значений параметров, заданных по умолчанию
+
+
+# @strict
+# def sum_two(a: int, b: int) -> int:
+#     return a + b
+# print(sum_two(1, 2))  # >>> 3
+# print(sum_two(1, 2.4))  # >>> TypeError
+
+def strict(func):
+    def wrapper(*args):
+        annotations = list(func.__annotations__.values())
+        default_annotations = annotations[:-1]
+        args_annotations = [type(arg) for arg in args]
+        if default_annotations == args_annotations:
+            return func(*args)
+        else:
+            raise TypeError
+    return wrapper
---
+++ task2/solution.py
@@ -0,0 +1,44 @@
+# Необходимо реализовать скрипт,
+# который будет получать с русскоязычной википедии список всех животных
+# (https://ru.wikipedia.org/wiki/Категория:Животные_по_алфавиту)
+# и записывать в файл в формате beasts.csv количество животных на каждую букву алфавита.
+# Содержимое результирующего файла:
+
+# А,642
+# Б,412
+# В,....
+
+# Примечание:
+# анализ текста производить не нужно,
+# считается любая запись из категории
+# (в ней может быть не только название, но и, например, род)
+
+
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+
+URL = 'https://ru.wikipedia.org/wiki/Категория:Животные_по_алфавиту'
+
+
+def write_to_scv(*args):
+    with open('task2/beasts.csv', 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(args)
+
+
+def get_html(url: str) -> str:
+    response = requests.get(url)
+    return response.text
+
+
+def get_data_from_first_page(url):
+    page = get_html(url)
+    soup = BeautifulSoup(page, 'html.parser')
+    div_tag = soup.find('div', 'mw-category mw-category-columns').find_all('div', 'mw-category-group')
+    data = {tag.find('h3').text: len(tag.find_all('li')) for tag in div_tag}
+    next_page = soup.find(string='Следующая страница').parent.get('href')
+    next_page_url = 'https://ru.wikipedia.org' + next_page
+    return data, next_page_url
+
